ef9c82ea0e28531c81bed900115de5a73308175d
Shirodai <e135725@ie.u-ryukyu.ac.jp>
2014-12-04 11:22:23 +0900

    ver.12/4

--- firstTest/firstTest/ViewController.swift
+++ firstTest/firstTest/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myLabel2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         myLabel.text = "こんにちは！"
+        myLabel2.text = "さようなら"
     }
 
     override func didReceiveMemoryWarning() {
