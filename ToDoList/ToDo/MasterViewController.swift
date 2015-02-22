77fddf954328f8688bd373c59a9927b7b486de88
ASATH <e135706@gmail.com>
2015-02-22 19:44:28 +0900

    NULLlu commit

--- ToDoList/ToDo/MasterViewController.swift
+++ ToDoList/ToDo/MasterViewController.swift
@@ -14,6 +14,24 @@ class MasterViewController: UITableViewController, AddItemViewControllerDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // UIImageViewを作成する.
+        let myImageView: UIImageView = UIImageView(frame: CGRectMake(0,0,302,500))
+        
+        // 表示する画像を設定する.
+        let myImage = UIImage(named: "logo8.png")
+        
+        // 画像をUIImageViewに設定する.
+        myImageView.image = myImage
+        
+        // 画像の表示する座標を指定する.
+        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 251.0)
+        
+        // UIImageViewをViewに追加する.
+        self.view.addSubview(myImageView)
+        
+        self.view.sendSubviewToBack(myImageView)
+        
         NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("frag"), userInfo: nil, repeats: true)
          NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("stt"), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
@@ -106,9 +124,6 @@ class MasterViewController: UITableViewController, AddItemViewControllerDelegate
                 appdelegate.lastask = array[回数]
                 Alert()
                 array2.removeAtIndex(回数)
-                
-                
-                
                 
             }
             回数++
